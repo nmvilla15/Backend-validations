31aa3e89973f5d0e03383e363d541476a2032a76
nmvilla15 <niki.villalpando@gmail.com>
2021-05-30 00:02:35 -0500

    halfway through spec tests

--- spec/requests/orders_spec.rb
+++ spec/requests/orders_spec.rb
@@ -1,17 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe "Orders", type: :request do
-  describe "GET /new" do
-    it "returns http success" do
-      get "/orders/new"
-      expect(response).to have_http_status(:success)
+  describe "get orders_path" do
+    it "renders the index view" do
+      FactoryBot.create_list(:order, 10)
+      get orders_path
+      expect(response).to render_template(:index)
     end
   end
 
-  describe "GET /index" do
-    it "returns http success" do
-      get "/orders/index"
-      expect(response).to have_http_status(:success)
+  describe "get order_path" do
+    it "renders the :show template" do
+      order = FactoryBot.create(:order)
+      get order_path(id: order.id)
+      expect(response).to render_template(:show)
+    end
+    it "redirects to the index path if the order id is invalid" do
+      get order_path(id: 5000)
+      expect(response).to redirect_to orders_path
     end
   end
 
